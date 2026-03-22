c12a87bc8dd3bd4d5f5ccaa1cdf41c10b6cd10dc
Rahul Raj Soren <rahulsoren90@gmail.com>
2026-03-23 04:10:00 +0530

    BLOCK-7 : Major rework in navbar; removed tailwind dependencies and shifted to css properties for styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import SolutionsMarketplace from "@/components/solutionmarket/landing"
 import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    redirect('/auth'))
+  redirect('/auth');
 }
